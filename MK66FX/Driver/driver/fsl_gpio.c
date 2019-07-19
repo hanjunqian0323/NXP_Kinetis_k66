60d1f0ae00eaeeac4d9228f0cfa3ee8fa8fb675a
王翀 <1225093964@qq.com>
2019-07-19 20:19:04 +0800

    添加GPIO的相关驱动

--- MK66FX/Driver/driver/fsl_gpio.c
+++ MK66FX/Driver/driver/fsl_gpio.c
@@ -1,12 +1,38 @@
 #include "fsl_gpio.h"
 
 
-
 uint8_t gpio_init(GPIO_Type *PTn,uint32_t pin,uint8_t dir)
 {
-	GPIO_PDOR_REG(PTn);
-
+	PORT_Type *portx;
+	
+	if		(PTn == PTA)  	portx = PORTA;
+	else if	(PTn == PTB)	portx = PORTB;
+	else if	(PTn == PTC)	portx = PORTC;
+	else if	(PTn == PTD)	portx = PORTD;
+	else if	(PTn == PTE)	portx = PORTE;
+	
+	port_init(portx,pin,001);
+	
+	//输入输出模式配置
+	if(dir == 0) //配置为输入模式
+	{
+		GPIO_PDDR_REG(PTn) &=~ (1<<pin);
+	}
+	else if(dir == 1)  //配置为输出模式
+	{
+		GPIO_PDDR_REG(PTn) |=  (1<<pin);
+	}
 }
 
-
+void gpio_set(GPIO_Type *PTn,uint32_t pin,uint8_t status)
+{	
+	if(status == 0)
+	{
+		GPIO_PDDR_REG(PTn) &=~ (1<<pin);
+	}
+	else if(status == 1)
+	{
+		GPIO_PDDR_REG(PTn) |=  (1<<pin);
+	}
+}
 
